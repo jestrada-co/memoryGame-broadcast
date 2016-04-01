25a5ee3fe50a1813f0084b734c8e5b3842d001b0
Jorge Estrada <co.jestrada@gmail.com>
2016-04-01 01:10:59 -0500

    Falta que el otro jugador pueda hacer match

---
+++ memoryGame-broadcast/memoryGame-broadcast/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace memoryGame_broadcast
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.imgListGame1 = new System.Windows.Forms.ImageList(this.components);
+            this.imgListApp = new System.Windows.Forms.ImageList(this.components);
+            this.imgListNone = new System.Windows.Forms.ImageList(this.components);
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtPlayers = new System.Windows.Forms.TextBox();
+            this.imgListGame2 = new System.Windows.Forms.ImageList(this.components);
+            this.btnOcultarImages = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // imgListGame1
+            // 
+            this.imgListGame1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListGame1.ImageStream")));
+            this.imgListGame1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListGame1.Images.SetKeyName(0, "acrobat.png");
+            this.imgListGame1.Images.SetKeyName(1, "android.png");
+            this.imgListGame1.Images.SetKeyName(2, "apple.png");
+            this.imgListGame1.Images.SetKeyName(3, "css.png");
+            this.imgListGame1.Images.SetKeyName(4, "excel.png");
+            this.imgListGame1.Images.SetKeyName(5, "facebook.png");
+            this.imgListGame1.Images.SetKeyName(6, "github.png");
+            this.imgListGame1.Images.SetKeyName(7, "google.png");
+            this.imgListGame1.Images.SetKeyName(8, "html.png");
+            this.imgListGame1.Images.SetKeyName(9, "linkedin.png");
+            this.imgListGame1.Images.SetKeyName(10, "linux.png");
+            this.imgListGame1.Images.SetKeyName(11, "pc.png");
+            this.imgListGame1.Images.SetKeyName(12, "twitter.png");
+            this.imgListGame1.Images.SetKeyName(13, "whatsap.png");
+            this.imgListGame1.Images.SetKeyName(14, "windows.png");
+            this.imgListGame1.Images.SetKeyName(15, "word.png");
+            this.imgListGame1.Images.SetKeyName(16, "wordpress.png");
+            this.imgListGame1.Images.SetKeyName(17, "youtube.png");
+            // 
+            // imgListApp
+            // 
+            this.imgListApp.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListApp.ImageStream")));
+            this.imgListApp.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListApp.Images.SetKeyName(0, "cerrar.png");
+            this.imgListApp.Images.SetKeyName(1, "puzzle.png");
+            // 
+            // imgListNone
+            // 
+            this.imgListNone.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListNone.ImageStream")));
+            this.imgListNone.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListNone.Images.SetKeyName(0, "humano.png");
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.BackColor = System.Drawing.Color.White;
+            this.btnStartGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartGame.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartGame.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btnStartGame.Image = ((System.Drawing.Image)(resources.GetObject("btnStartGame.Image")));
+            this.btnStartGame.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStartGame.Location = new System.Drawing.Point(12, 12);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(211, 40);
+            this.btnStartGame.TabIndex = 1;
+            this.btnStartGame.Text = "StartGame Enviar Invitación";
+            this.btnStartGame.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStartGame.UseVisualStyleBackColor = false;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            this.btnStartGame.DragLeave += new System.EventHandler(this.btnStartGame_DragLeave);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Red;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(483, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(81, 40);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Salir";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtPlayers
+            // 
+            this.txtPlayers.Location = new System.Drawing.Point(570, 58);
+            this.txtPlayers.Multiline = true;
+            this.txtPlayers.Name = "txtPlayers";
+            this.txtPlayers.Size = new System.Drawing.Size(191, 164);
+            this.txtPlayers.TabIndex = 3;
+            // 
+            // imgListGame2
+            // 
+            this.imgListGame2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListGame2.ImageStream")));
+            this.imgListGame2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListGame2.Images.SetKeyName(0, "acrobat.png");
+            this.imgListGame2.Images.SetKeyName(1, "android.png");
+            this.imgListGame2.Images.SetKeyName(2, "apple.png");
+            this.imgListGame2.Images.SetKeyName(3, "css.png");
+            this.imgListGame2.Images.SetKeyName(4, "excel.png");
+            this.imgListGame2.Images.SetKeyName(5, "facebook.png");
+            this.imgListGame2.Images.SetKeyName(6, "github.png");
+            this.imgListGame2.Images.SetKeyName(7, "google.png");
+            this.imgListGame2.Images.SetKeyName(8, "html.png");
+            this.imgListGame2.Images.SetKeyName(9, "linkedin.png");
+            this.imgListGame2.Images.SetKeyName(10, "linux.png");
+            this.imgListGame2.Images.SetKeyName(11, "pc.png");
+            this.imgListGame2.Images.SetKeyName(12, "twitter.png");
+            this.imgListGame2.Images.SetKeyName(13, "whatsap.png");
+            this.imgListGame2.Images.SetKeyName(14, "windows.png");
+            this.imgListGame2.Images.SetKeyName(15, "word.png");
+            this.imgListGame2.Images.SetKeyName(16, "wordpress.png");
+            this.imgListGame2.Images.SetKeyName(17, "youtube.png");
+            // 
+            // btnOcultarImages
+            // 
+            this.btnOcultarImages.BackColor = System.Drawing.Color.White;
+            this.btnOcultarImages.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOcultarImages.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOcultarImages.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btnOcultarImages.Image = ((System.Drawing.Image)(resources.GetObject("btnOcultarImages.Image")));
+            this.btnOcultarImages.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOcultarImages.Location = new System.Drawing.Point(238, 12);
+            this.btnOcultarImages.Name = "btnOcultarImages";
+            this.btnOcultarImages.Size = new System.Drawing.Size(150, 40);
+            this.btnOcultarImages.TabIndex = 1;
+            this.btnOcultarImages.Text = "Ocultar Imagenes";
+            this.btnOcultarImages.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOcultarImages.UseVisualStyleBackColor = false;
+            this.btnOcultarImages.Click += new System.EventHandler(this.btnOcultarImages_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 551);
+            this.panel1.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 621);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtPlayers);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOcultarImages);
+            this.Controls.Add(this.btnStartGame);
+            this.Name = "frmMain";
+            this.Text = "MemoryGame Broadcast";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imgListGame1;
+        private System.Windows.Forms.ImageList imgListApp;
+        private System.Windows.Forms.ImageList imgListNone;
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtPlayers;
+        private System.Windows.Forms.ImageList imgListGame2;
+        private System.Windows.Forms.Button btnOcultarImages;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ memoryGame-broadcast/memoryGame-broadcast/Form1.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace memoryGame_broadcast
+{
+    public partial class frmMain : Form
+    {
+        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        Thread hiloReceiveData;
+        string strCommand = "";
+        string dirIp;
+        string dirIpOponente;
+        bool swReceiveData = true;
+        bool starter = false;
+        Image[,] matImages = new Image[6, 6];
+        Button[,] matButtons = new Button[6, 6];
+        int[,] matMatch = new int[6, 6];
+        int num = 0;
+        List<int> rango1 = new List<int>();
+        List<int> rango2 = new List<int>();
+        string tablero = "";
+        int intPuertoDestino;
+        int intIndice = 0;
+        int x = 6;
+        int y = 6;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+            int intPuertoLocal;
+            DialogResult TipoGame = MessageBox.Show("Desea realizar una simulación local del juego?","Seleccinar tipo de Juego",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (TipoGame  == DialogResult.Yes)
+            {
+                string strPuerto = Microsoft.VisualBasic.Interaction.InputBox("Digite el No del Puerto Local...", "Configurar simulacón...", "9000");
+                intPuertoLocal = Convert.ToInt16(strPuerto);
+                strPuerto = Microsoft.VisualBasic.Interaction.InputBox("Digite el No del Puerto Destino...", "Configurar simulacón...", "9000");
+                intPuertoDestino = Convert.ToInt16(strPuerto);
+            }
+            else
+            {
+                intPuertoLocal = 9000;
+                intPuertoDestino = 8000;
+            }
+
+            construirTablero();
+            OcultarImages();
+            sock.Bind(new IPEndPoint(obtenerIPLocal(), intPuertoLocal));
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+            hiloReceiveData = new Thread(receiveData);
+            hiloReceiveData.Start();
+        }
+
+        private IPAddress obtenerIPLocal()
+        {
+            IPHostEntry host;
+            IPAddress localIP = null;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIP = ip;
+                }
+            }
+            return localIP;
+        }
+
+        private void receiveData()
+        {
+            while (swReceiveData)
+            {
+                IPEndPoint ipRemota = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint ipRecibida = (EndPoint)ipRemota;
+                byte[] bytesRecibidos = new byte[256];
+                sock.ReceiveFrom(bytesRecibidos, bytesRecibidos.Length, SocketFlags.None, ref ipRecibida);
+                strCommand = Encoding.Default.GetString(bytesRecibidos);
+                dirIp = ((IPEndPoint)ipRecibida).Address.ToString();
+                ejecutor();
+            }
+        }
+
+        private void ejecutor()
+        {
+            // 0 Recibir Invitación a nuevo juego
+            // 1 no Acepto invitación u Ocupado jugando
+            // 2 Acepto invitación
+            // 3 partida cancelada por el otro jugador, tu ganas
+            // 4 jugada
+            // 5
+
+            int intCommand = Convert.ToInt16(strCommand.Substring(0, 1));
+
+            if (intCommand == 0)
+            {
+                if (starter)
+                {
+                    strCommand = "1";
+                }
+                else
+                {
+                    DialogResult aceptarGame = MessageBox.Show("El jugador con IP " + dirIp + " te ha invitado a un Game. Deseas Aceptar?", "Invitación a un Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (aceptarGame == DialogResult.Yes)
+                    {
+                        strCommand = "2";
+                        dirIpOponente = dirIp;
+                        starter = true;
+                        //btnStartGame.Enabled = false;
+                        randomImages();
+                        //MostrarImages();
+                        strCommand = strCommand + tablero;
+                    }
+                    else
+                    {
+                        strCommand = "1";
+                    }
+                }
+                enviarCommand();
+            }
+            else
+            {
+                if (intCommand == 1)
+                {
+                    MessageBox.Show("El jugador con IP " + dirIpOponente + " no aceptó la invitación o ya se encuentra jugando!", "Invitación a un Game",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    if (intCommand == 2)
+                    {
+                        //btnStartGame.Enabled = false;
+                        starter = true;
+                        dirIpOponente = dirIp;
+                        MessageBox.Show("El jugador con IP " + dirIpOponente + " aceptó la invitación. Ahora es tu turno!", "Juego en curso...", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        replicarRandomImages();
+                        //MostrarImages();
+                    }
+                    else
+                    {
+                        if (intCommand == 3)
+                        {
+                            MessageBox.Show("el otro jugador canceló la partida. tu ganas");
+                            starter = false;
+                        }
+                        else
+                        {
+                            if (intCommand == 4)
+                            {
+                                if(dirIpOponente.CompareTo(dirIp)== 0)
+                                {
+                                    jugada();
+                                }
+                            }
+                            else
+                            {
+                                if (intCommand == 5)
+                                {
+
+                                }
+                                else
+                                {
+                                    if (intCommand == 6)
+                                    {
+                                        matButtons[Convert.ToInt16(strCommand.Substring(1, 1)), Convert.ToInt16(strCommand.Substring(2, 1))].Image = imgListNone.Images[0];
+                                        matButtons[Convert.ToInt16(strCommand.Substring(3, 1)), Convert.ToInt16(strCommand.Substring(4, 1))].Image = imgListNone.Images[0];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void jugada()
+        {
+            int i = Convert.ToInt16(strCommand.Substring(1, 1));
+            int j = Convert.ToInt16(strCommand.Substring(2, 1));
+            matButtons[i, j].Image = matImages[i, j];
+        }
+
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            terminarApp();
+        }
+
+        private void terminarApp()
+        {
+            swReceiveData = false;
+            sock.Close();
+            hiloReceiveData.Abort();
+        }
+
+        private void construirTablero()
+        {
+                int tabIndex = 1;
+                int top = 13;
+                int left = 0;
+
+                for (int i = 0; i <= 5; i++)
+                {
+                    left = 13;
+                    for (int j = 0; j <= 5; j++)
+                    {
+                        matButtons[i, j] = new Button();
+                        matButtons[i, j].BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+                        matButtons[i, j].Cursor = System.Windows.Forms.Cursors.Hand;
+                        matButtons[i, j].FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+                        matButtons[i, j].FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+                        matButtons[i, j].FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+                        matButtons[i, j].FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                        matButtons[i, j].Location = new System.Drawing.Point(top, left);
+                        matButtons[i, j].Name = i.ToString() + j.ToString();
+                        matButtons[i, j].Size = new System.Drawing.Size(74, 74);
+                        matButtons[i, j].TabIndex = tabIndex;
+                        matButtons[i, j].UseVisualStyleBackColor = true;
+                        matButtons[i, j].Click += new System.EventHandler(matButtons_Click);
+
+                        panel1.Controls.Add(matButtons[i, j]);
+                        tabIndex++;
+                        left = left + 90;
+                    }
+                    top = top + 90;
+                }
+        }
+
+        public void replicarRandomImages()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    int num = obtenerIndice();
+                    matImages[i, j] = imgListGame1.Images[num];
+                    matMatch[i, j] = num;
+                }
+            }
+        }
+
+        private int obtenerIndice()
+        {
+            intIndice++;
+            string strTemp = strCommand.Substring(intIndice, 1);
+            intIndice++;
+            if (strCommand.Substring(intIndice,1).CompareTo("@") != 0)
+            {
+                strTemp=strTemp+ strCommand.Substring(intIndice, 1);
+                intIndice++;
+            }
+            return (Convert.ToInt16(strTemp));
+        }
+
+        private void matButtons_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            strCommand = "4" + b.Name;
+            int i = Convert.ToInt16(b.Name.Substring(0, 1));
+            int j = Convert.ToInt16(b.Name.Substring(1, 1));
+            matButtons[i, j].Image = matImages[i, j];
+            enviarCommand();
+
+            if ((x != 6) && (y != 6))
+            {
+                bool sw = true;
+                if ((x == i) && (y == j))
+                {
+                    sw = false;
+                }
+                if ((matMatch[i,j] == matMatch[x, y]) && sw )
+                {
+                    MessageBox.Show("Match");
+                    strCommand = "5" + i.ToString() + j.ToString() + x.ToString() + y.ToString();
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    matButtons[x, y].Image = imgListNone.Images[0];
+                    matButtons[i, j].Image = imgListNone.Images[0];
+                    strCommand = "6" + i.ToString() + j.ToString() + x.ToString() + y.ToString();
+                }
+                enviarCommand();
+                x = 6;
+                y = 6;
+            }
+            else
+            {
+                x = i;
+                y = j;
+            }
+        }
+
+        private void randomImages()
+        {
+            for (int i = 0; i<= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    Random r = new Random();
+                    if (rango1.Count >= 18)
+                    {
+                        num = 2;
+                    }
+                    else
+                    {
+                        if (rango2.Count >= 18)
+                        {
+                            num = 1;
+                        }
+                        else
+                        {
+                            num = r.Next(1, 3);
+                        }
+                    }
+
+                    if (num == 1)
+                    {
+                        num = r.Next(0, 18);
+                        while (rango1.Contains(num))// verificamos números repetidos
+                        {
+                            num = r.Next(0, 18);
+                        }
+                        rango1.Add(num);
+                        matImages[i, j] = imgListGame1.Images[num];
+                        matMatch[i, j] = num;
+                        tablero = tablero + num.ToString() + "@";
+                    }
+                    else
+                    {
+                        if (num == 2)
+                        {
+                            num = r.Next(0, 18);
+                            while (rango2.Contains(num))// verificamos números repetidos
+                            {
+                                num = r.Next(0, 18);
+                            }
+                            rango2.Add(num);
+                            matImages[i, j] = imgListGame2.Images[num];
+                            tablero = tablero + num.ToString() + "@";
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void OcultarImages()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    matButtons[i, j].Image = imgListNone.Images[0];
+                }
+            }
+        }
+
+        private void MostrarImages()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    matButtons[i, j].Image = matImages[i, j];
+                }
+            }
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            strCommand = "0";
+            enviarCommand();
+        }
+
+        private void enviarCommand()
+        {
+            IPEndPoint ipDestino = new IPEndPoint(IPAddress.Broadcast, intPuertoDestino);
+            byte[] bytesEnviar = Encoding.Default.GetBytes(strCommand);
+            sock.SendTo(bytesEnviar, bytesEnviar.Length, SocketFlags.None, ipDestino);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            terminarApp();
+            Application.Exit();
+        }
+
+        private void btnOcultarImages_Click(object sender, EventArgs e)
+        {
+            OcultarImages();
+        }
+
+        private void btnStartGame_DragLeave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
